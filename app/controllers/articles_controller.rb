e4b88789753da7881887075a6048c988409da0fe
Simon Debbarma <simonpeterdebbarma@outlook.com>
2021-05-04 13:16:28 +0100

    create and implement delete action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -34,6 +34,13 @@ class ArticlesController < ApplicationController
       render :edit
     end
   end
+  
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path
+  end
 
   private
     def article_params
