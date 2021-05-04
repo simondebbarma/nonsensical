d8ee23688bbe18133bab076ede45c129a8c5a243
Simon Debbarma <simonpeterdebbarma@outlook.com>
2021-05-04 15:42:13 +0530

    fix a routing error

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root 'articles#index'
+  root "articles#index"
 
-  get '/articles', to: 'articles#index'
+  get "/articles", to: "articles#index"
   get "/articles/:id", to: "articles#show"
 end
